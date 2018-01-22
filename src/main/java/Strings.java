6697410ced05c087ac22eccd15bf22cb1aff3841
Oleg Mihaylov <oleg.mihaylov@gmail.com>
2018-01-22 11:57:12 +0300

    add countDigits

--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -1,6 +1,6 @@
 public class Strings {
     public static int countDigits(String string){
-        int result = 0;
+        int result;
         for (int i = 0; i < string.length(); i++)
             if(Character.isDigit(string.charAt(i))) result ++;
         return result;
---
+++ src/test/java/StringsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringsTest {
+
+    @Test
+    public void countDigits() {
+    }
+}
