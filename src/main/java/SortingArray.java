46d6c8098c3da7fd58c804fe863ef255b9ced842
Oleg Mihaylov <oleg.mihaylov@gmail.com>
2018-01-23 10:41:21 +0300

    Add bubbleSort

---
+++ src/main/java/SortingArray.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+public class SortingArray{
+    /**
+     *
+     * @param comparables -- ArrayList to sorting
+     * @param order -- ascending (default) or re descending order
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable> ArrayList<T> bubbleSort(ArrayList<T> comparables, boolean order){
+        int size = comparables.size();
+        ArrayList<T> result = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+            result.add(comparables.get(i));
+        for (int i = 0; i < size-1; i++) {
+            for (int j = 0; j < size - i -1; j++) {
+                if (result.get(j).compareTo(result.get(j + 1)) > 0)
+                    result.set(j,result.set(j+1,result.get(j)));
+            }
+        }
+        return result;
+    }
+
+    public static <T extends Comparable> ArrayList<T> bubbleSort(ArrayList<T> comparables){
+        return bubbleSort(comparables, true);
+    }
+}
---
+++ src/test/java/SortingArrayTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class SortingArrayTest {
+
+    @Test
+    public void bubbleSort(){
+        int size = 10;
+        ArrayList<Integer> arrayList = new ArrayList<>(size);
+        for (int i = 0; i < size; i++)
+            arrayList.add(size - i);
+        ArrayList<Integer> sorted = new SortingArray().bubbleSort(arrayList);
+        for (int i = 0; i < size-1; i++)
+            assertFalse(sorted.get(i).compareTo(sorted.get(i+1)) > 0);
+        System.out.printf("%n");
+    }
+}
