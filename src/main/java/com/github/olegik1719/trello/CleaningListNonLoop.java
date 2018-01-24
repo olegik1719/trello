e3031967e5a32dcf0b424d6618768bbba91bf6aa
Oleg Mihaylov <oleg.mihaylov@gmail.com>
2018-01-24 16:52:34 +0300

    Without recursive, but without order...

--- src/main/java/com/github/olegik1719/trello/CleaningListNonLoop.java
+++ src/main/java/com/github/olegik1719/trello/CleaningListNonLoop.java
@@ -7,26 +7,7 @@ import java.util.Set;
 
 public class CleaningListNonLoop <T>{
 
-    private List<T> result;
-    private List<T> source;
-    private Set<T> objects;
-
-    private CleaningListNonLoop(List<T> source){
-            result = new ArrayList<>();
-            this.source = source;
-            objects = new HashSet<>();
-    }
-
-    private List<T> cleanIt(int index){
-        if (index == source.size())
-            return result;
-        T current = source.get(index);
-        if (objects.add(current))
-            result.add(current);
-        return cleanIt(++index);
-    }
-
     public static <T> List<T> removeDuplicates(List<T> source){
-        return new CleaningListNonLoop<T>(source).cleanIt(0);
+        return new ArrayList(new HashSet(source));
     }
 }
