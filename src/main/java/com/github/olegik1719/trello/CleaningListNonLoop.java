e700c129644707c3c1b7a0045a7c874209aaff71
Oleg Mihaylov <oleg.mihaylov@gmail.com>
2018-01-24 17:44:23 +0300

    just One stream

--- src/main/java/com/github/olegik1719/trello/CleaningListNonLoop.java
+++ src/main/java/com/github/olegik1719/trello/CleaningListNonLoop.java
@@ -1,13 +1,14 @@
 package com.github.olegik1719.trello;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CleaningListNonLoop <T>{
 
     public static <T> List<T> removeDuplicates(List<T> source){
-        return new ArrayList(new HashSet(source));
+
+        return new HashSet<>(source).stream()
+                .sorted(Comparator.comparingInt(source::indexOf))
+                .collect(Collectors.toList());
     }
 }
