f2c12b70467e289bf049e40494f6fca4b9856763
Oleg Mihaylov <oleg.mihaylov@gmail.com>
2018-01-26 08:54:49 +0300

    Made One Class -- ListUtils, with 2 static methods(Loop, NonLoop). Update DataManagerTest: try-with-resources.

--- src/main/java/com/github/olegik1719/trello/ListUtils.java
+++ src/main/java/com/github/olegik1719/trello/ListUtils.java
@@ -2,14 +2,20 @@ package com.github.olegik1719.trello;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class CleaningList {
-    public static <T> ArrayList<T> RemoveDoubles(ArrayList<T> dataArray){
+public class ListUtils {
+    public static <T> List<T> removeDuplicates(List<T> dataArray){
         ArrayList<T> result = new ArrayList<>();
         HashSet<T> alones = new HashSet<>();
         for (T t:dataArray)
             if (alones.add(t))
                 result.add(t);
         return result;
+    }
+
+    public static <T> List<T> removeDuplicatesNonLoop(List<T> source){
+        return source.stream().distinct().collect(Collectors.toList());
     }
 }
---
+++ src/test/java/com/github/olegik1719/trello/ListUtilsTest.java
@@ -0,0 +1,49 @@
+package com.github.olegik1719.trello;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ListUtilsTest {
+
+    List<Integer> arrayList;
+    List<Integer> cleanedList;
+
+    @Before
+    public void setUp() throws Exception {
+        int max = 10;
+        arrayList = new ArrayList<>();
+        for (int i = 0; i < max; i++) {
+            int r = (int)(java.lang.Math.random()*100);
+            for (int j = 0; j < i; j++) arrayList.add(r);
+        }
+
+        for (int i: arrayList) {
+            System.out.printf("%s ", i);
+        }
+        System.out.printf("%n");
+    }
+
+    @Test
+    public void removeDuplicates() {
+        cleanedList = ListUtils.removeDuplicates(arrayList);
+    }
+
+    @Test
+    public void removeDuplicatesNonLoop() {
+        cleanedList = ListUtils.removeDuplicatesNonLoop(arrayList);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        for (int i: cleanedList) {
+            System.out.printf("%s ", i);
+        }
+        System.out.printf("%n");
+    }
+}
