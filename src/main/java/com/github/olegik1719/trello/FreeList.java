be4c7ce136cfeb3696229095fb021b821360b612
olegik1719 <oleg.mihaylov@gmail.com>
2018-01-31 01:10:23 +0300

    Change method grow; Add message to IndexOutOfBoundsException(); Small changes to test (assert creation put in begin to all tests) Change get: if you want get smth in after indexes, you get null, but list doesn't change self size.

--- src/main/java/com/github/olegik1719/trello/FreeList.java
+++ src/main/java/com/github/olegik1719/trello/FreeList.java
@@ -7,15 +7,16 @@ public class FreeList<E> extends ArrayList <E> {
 
 
     private boolean rangeCheck(int index){
-        if (index < 0) throw new IndexOutOfBoundsException();
+        if (index < 0) throw new IndexOutOfBoundsException("Index must be positive!");
         if (index > super.size()) return true;
         return false;
     }
 
-    private FreeList grow(int size){
-        if (size >= super.size()){
-            super.ensureCapacity(size+1);
-            for (int i = super.size(); i < size + 1; i++) {
+    private FreeList grow(int toIndex){
+        if (toIndex < 0) throw new IndexOutOfBoundsException("Index must be positive!");
+        if (toIndex >= super.size()){
+            super.ensureCapacity(toIndex+1);
+            for (int i = super.size(); i < toIndex + 1; i++) {
                 super.add(null);
             }
         }
@@ -32,24 +33,24 @@ public class FreeList<E> extends ArrayList <E> {
 
     public FreeList(int beginingSize){
         super(beginingSize);
-        if (rangeCheck(beginingSize-1)) grow(beginingSize-1) ;// grow
+        grow(beginingSize-1) ;// grow
     }
 
     @Override
     public E get(int index) {
-        if (rangeCheck(index)) grow(index);
+        if (rangeCheck(index)) return null;
         return super.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        if (rangeCheck(index)) grow(index);
+        grow(index);
         return super.set(index,element);
     }
 
     @Override
     public void add(int index, E element) {
-        if (rangeCheck(index-1)) grow(index-1);
+        grow(index-1);
         super.add(index,element);
     }
 
@@ -63,7 +64,7 @@ public class FreeList<E> extends ArrayList <E> {
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        if (rangeCheck(index-1)) grow(index-1);
+        grow(index-1);
         return super.addAll(index,c);
     }
 }
--- src/test/java/com/github/olegik1719/trello/FreeListTest.java
+++ src/test/java/com/github/olegik1719/trello/FreeListTest.java
@@ -12,29 +12,32 @@ public class FreeListTest {
     @Before
     public void setUp() throws Exception {
         freeList = new FreeList<>(1_000);
-        assertEquals(1_000,freeList.size());
     }
 
     @Test
     public void get() {
+        assertEquals(1_000,freeList.size());
         assertNull(freeList.get(999_999));
         assertEquals(1_000_000,freeList.size());
     }
 
     @Test
     public void set() {
+        assertEquals(1_000,freeList.size());
         assertNull(freeList.set(999_999, 1_000));
-        assertEquals(1_000_000,freeList.size());
+        assertEquals(1_000,freeList.size());
     }
 
     @Test
     public void remove() {
+        assertEquals(1_000,freeList.size());
         assertNull(freeList.remove(999_999));
-        assertEquals(1000,freeList.size());
+        assertEquals(1_000,freeList.size());
     }
 
     @Test
     public void add() {
+        assertEquals(1_000,freeList.size());
         freeList.add(999_999, 1_000);
         assertEquals(freeList.size(),1_000_000);
         assertEquals(1_000, (Object)freeList.get(999_999));
